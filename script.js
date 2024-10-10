373af40c782e353980c575d3f56f4776faf9074e
Gisele Lais <105565774+ei-Gih@users.noreply.github.com>
2024-10-10 19:18:25 -0300

    Estilização
    Add style, e mudanças no js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const io = require('socket.io')(3000);
-
-io.on('connection', (socket) => {
-    socket.on('jogada', (dados) => {
-        // Enviar a jogada para o oponente
-        socket.broadcast.emit('jogadaOponente', dados);
-    });
-});
-
-
-
 function jogar(escolhaJogador) {
     const escolhas = ['pedra', 'papel', 'tesoura'];
     const escolhaIA = escolhas[Math.floor(Math.random() * 3)];
+
+    // Esconde a imagem padrão e exibe as escolhas do jogador e do computador
+    document.getElementById('default-choice').style.display = 'none';
+    document.getElementById('computer-choice').style.display = 'block';
+    document.getElementById('player-choice').style.display = 'block';
+
+    // Atualiza as imagens para mostrar as escolhas do jogador e do computador
+    const imagemComputer = document.getElementById('imagem-computer');
+    const imagemJogador = document.getElementById('imagem-jogador');
+
+    imagemComputer.src = `img/${escolhaIA}.png`; // Mostra a escolha da IA usando o caminho correto da pasta
+    imagemComputer.alt = escolhaIA;
+
+    imagemJogador.src = `img/${escolhaJogador}.png`; // Mostra a escolha do jogador usando o caminho correto da pasta
+    imagemJogador.alt = escolhaJogador;
 
     let resultado = '';
 
@@ -27,5 +31,5 @@ function jogar(escolhaJogador) {
         resultado = 'Você perdeu!';
     }
 
-    document.getElementById('resultado').innerHTML = `Você escolheu: ${escolhaJogador} | IA escolheu: ${escolhaIA} <br> ${resultado}`;
+    document.getElementById('resultado').innerHTML = `${escolhaIA} venceu!`;
 }
